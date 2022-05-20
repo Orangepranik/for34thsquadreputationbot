5c714c8bf5f8b3d089070cae9da04f8b727602b0
valolikx <bundakilya@gmail.com>
2022-05-20 22:32:42 +0300

    fds

--- databasesqlite/database.py
+++ databasesqlite/database.py
@@ -4,5 +4,5 @@ base = sqlite3.connect("database1.db")
 cur = base.cursor()
 
 # base.execute("CREATE TABLE IF NOT EXISTS users(username VARCHAR(256),userid VARCHAR(300),phonenumber VARCHAR(30), userbio VARCHAR(150))")
-base.execute("CREATE TABLE IF NOT EXISTS reputation(username VARCHAR(256),userid VARCHAR(300),reputation VARCHAR(10000)")
+base.execute("CREATE TABLE IF NOT EXISTS reputation(username VARCHAR(256),userid VARCHAR(300),reputation VARCHAR(10000))")
 base.commit()
--- main.py
+++ main.py
@@ -6,6 +6,10 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 storage=MemoryStorage()
 dp = Dispatcher(bot, storage = storage)
+
+
+start_reputation = 0
+
 uprage_reputation=['+','спасибо','рэхмэт','годно','рэхмэт','годнота','годно','охуенно','хорошо','круто','супер','гуд', 'гут', 'гут', 'молодцы', 'молодец', 'няша', 'няши','милота','вылижу', 'няшнота', 'няха', 'няхи', 'цмок', 'ням', 'бох', 'отсосу', 'высосу']
 uprage_reputation_big=[]
 for bigwords in uprage_reputation:
@@ -21,7 +25,30 @@ async def upragereputationfunc(message: types.Message):
     idusereplyer = message.from_user.id
     idusergetreplayed = message.reply_to_message
     username_repleyer = message.from_user.username
-    idusergetreplayed1 = idusergetreplayed['from']['last_name']
-    cur.execute('INSERT INTO ')
+
+    id_user_get_replayed_1 = idusergetreplayed['from']['id']
+    username_user_get_replayed_1 = idusergetreplayed['from']['username']
+
+    checkid_1 = cur.execute('SELECT * FROM reputation WHERE userid==?', (idusereplyer,)).fetchone()
+    checkid_2 = cur.execute('SELECT * FROM reputation WHERE userid==?', (id_user_get_replayed_1,)).fetchone()
+    print(username_user_get_replayed_1, id_user_get_replayed_1)
+    if checkid_1 is None:
+        cur.execute("INSERT INTO reputation(username,userid,reputation) VALUES(?, ?, ?)", (username_repleyer, idusereplyer, start_reputation))
+        base.commit()
+    if checkid_2 is None:
+        cur.execute("INSERT INTO reputation(username,userid,reputation) VALUES(?, ?, ?)", (username_user_get_replayed_1, id_user_get_replayed_1, start_reputation))
+        base.commit()
+    getreputationuser_1 = checkid_1[2]
+    getreputationuser_2 = checkid_2[2]
+    print(getreputationuser_2)
+    int_getreputationuser_2 = int(float(getreputationuser_2))
+    reputation_up = int_getreputationuser_2+1
+    print(reputation_up)
+    await message.answer(f"{reputation_up}")
+    select_user = cur.execute("SELECT reputation FROM reputation WHERE userid==?", (id_user_get_replayed_1,)).fetchone()
+    update_reputation_up = ('UPDATE reputation SET reputation = ? WHERE userid =?')
+    infoupdate = (reputation_up, id_user_get_replayed_1)
+    cur.execute(update_reputation_up, infoupdate)
+    base.commit()
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
